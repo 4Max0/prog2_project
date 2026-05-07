c0f1d9ae2ed80f9a53c9defbc5b283318bc51d0d
Maxim Pass <4Max0@proton.me>
2026-05-07 13:56:41 +0200

    Verbessern und saeubern der main

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,35 +5,14 @@ public class App {
 	public String getGreeting() {
 		return "Hello World!";
 	}
+
 	public String getJavaVersion() {
 		return System.getProperty("java.version");
 	}
 
-	public String testSpotless() {String a="I'm u";String b="sing spotless!!" ;return a+b;}
-
-
-	public void x(){
-		System.out.println("hi");
-
-
-
-
-
-
-
-	}
-
-
-	public void y()
-	{
-	}
-
-
-
 	public static void main(String[] args) {
 		App a = new App();
-		System.out.println(a.getGreeting());
-		System.out.println(a.getJavaVersion());
-		System.out.println(a.testSpotless());
+		IO.println(a.getGreeting());
+		IO.println(a.getJavaVersion());
 	}
 }
